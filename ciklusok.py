761a394ac3e2a829383077cb0f83a836cbcff94e
Margareta <margaretavida120@gmail.com>
2021-10-17 15:25:03 +0200

    7.feladat2.próba

--- ciklusok.py
+++ ciklusok.py
@@ -84,9 +84,16 @@ for i in range(a,b):
 
 n= int(input("Kérek egy számot!"))
 
-for i in range(2,n):
-    num=(i*i-i)
-print("n!=",num)
+if n < 0:
+        return 0
+    elif n == 0 or n == 1:
+        return 1
+    else:
+        fact = 1
+        while(n > 1):
+            fact *= n
+            n -= 1
+        return fact
 
 #44.feladat:
 """
